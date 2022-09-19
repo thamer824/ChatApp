0c39acf63412bc9448d044b38c08563f25780ceb
Thamer Galai Zar <thameur.kalaizar@medtech.tn>
2022-09-19 19:26:40 +0200

    make it betterd

--- index.js
+++ index.js
@@ -31,12 +31,10 @@ const mongo = async()=>{
 } 
 mongo();
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
+  });
 const PORT = process.env.PORT || 5000
 const server = app.listen(PORT, () =>{
     console.log('Server is running on port', PORT)
