3aeead6bdea59070f9aa94b07aa018e447d180e1
Thamer Galai Zar <thameur.kalaizar@medtech.tn>
2022-09-19 19:46:53 +0200

    make it better

--- server.js
+++ server.js
@@ -30,12 +30,15 @@ const mongo = async()=>{
     }
 } 
 mongo();
+
+
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
+
 const PORT = process.env.PORT || 5000
 const server = app.listen(PORT, () =>{
     console.log('Server is running on port', PORT)
